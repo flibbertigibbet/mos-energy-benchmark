0ec6ae2d3a2393c5d60ac1244aeab1f22da9ce5c
Kathryn Killebrew <kkillebrew@azavea.com>
2014-12-05 17:57:01 -0500

    Use CartoDB.js to present viz.
    Removes angular-leaflet-directive; just use CartoDB.js instead.

--- app/scripts/views/map/map-controller.js
+++ app/scripts/views/map/map-controller.js
@@ -4,12 +4,13 @@
     /*
      * ngInject
      */
-    function MapController($scope) {
-        angular.extend($scope, {
-            defaults: {
-                scrollWheelZoom: false
-            }
-        });
+    function MapController($scope, $timeout) {
+
+        cartodb.createVis('mymap', 'http://azavea-demo.cartodb.com/api/v2/viz/d9998c20-7bf6-11e4-b489-0e853d047bba/viz.json')
+            .done(function(vis, layers) {
+                
+            });
+
     }
 
     angular.module('mos.views.map')
