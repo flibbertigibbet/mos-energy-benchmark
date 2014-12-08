10faee1e808ee4d606780e2726a62d587673080d
Kathryn Killebrew <kkillebrew@azavea.com>
2014-12-08 09:39:03 -0500

    Add map and viz together

--- app/scripts/views/map/map-controller.js
+++ app/scripts/views/map/map-controller.js
@@ -6,10 +6,30 @@
      */
     function MapController($scope, $timeout) {
 
-        cartodb.createVis('mymap', 'http://azavea-demo.cartodb.com/api/v2/viz/d9998c20-7bf6-11e4-b489-0e853d047bba/viz.json')
+        // temp:  http://azavea-demo.cartodb.com/api/v2/viz/701a2d94-44f2-11e4-bb22-0e10bcd91c2b/viz.json
+        // age: http://azavea-demo.cartodb.com/api/v2/viz/d9998c20-7bf6-11e4-b489-0e853d047bba/viz.json
+
+        console.log('in map controller');
+        cartodb.createVis('mymap', 'http://azavea-demo.cartodb.com/api/v2/viz/701a2d94-44f2-11e4-bb22-0e10bcd91c2b/viz.json')
             .done(function(vis, layers) {
-                
+                console.log('viz created');
+                var nativeMap = vis.getNativeMap();
+                var overlay = layers[1];
+                console.log(nativeMap);
+                console.log(overlay);
+
+                //var sublayer = overlay.getSubLayer(0);
+                //overlay.infowindow.set('energy_star_score', '<p>foo!</p>');
+
+                var opts = {infowindowTemplate: '<p>foo!</p>',
+                            triggerEvent: 'featueHover'
+                            };
+
+                //var info = vis.addInfowindow(nativeMap, overlay, ['energy_star_score'], opts);
+                //console.log(info);
             });
+
+        //$timeout(function () { map.invalidateSize(); });
 
     }
 
