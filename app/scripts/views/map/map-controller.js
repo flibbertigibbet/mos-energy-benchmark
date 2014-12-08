df4e7acc7a25d4bfac06d2bcdbc0c7a661d07a9a
Kathryn Killebrew <kkillebrew@azavea.com>
2014-12-08 16:26:27 -0500

    Use custom AngularJS popup for map and count compared items

--- app/scripts/views/map/map-controller.js
+++ app/scripts/views/map/map-controller.js
@@ -10,94 +10,93 @@
 
         // initialization
         $scope.compare_selections = [];  // array of up to three selections to compare
-        $scope.current_popup_id = -1;    // track the cartodb_id of the last feature clicked
+        $scope.haveThree = false;
 
+        // TODO: indicate that map is loading, hang on...
         console.log('in map controller; going to load map viz...');
 
         $scope.compare = function() {
-
+            console.log('go compare ' + $scope.cartodb_id);
+            if ($scope.haveThree) {
+                // shouldn't get here (button is disabled)
+                console.error('Cannot compare more than three items at a time');
+            } else {
+                // check to see if we have this one already
+                if ($scope.compare_selections.indexOf($scope.cartodb_id) > -1) {
+                    console.log('already have ' + $scope.cartodb_id + ' in comparison list');
+                } else {
+                    $scope.compare_selections.push($scope.cartodb_id);
+                    $scope.haveThree = $scope.compare_selections.length >=3 ? true : false;
+                }
+            }
         };
 
-        // compiled AngularJS HTML conflicts with Carto moustache templating
-        //var infowindow_click = $compile('<p><a onclick="foo()">Foo!</a></p>')($scope)[0];
-        var infowindow_template = '<span><div class="cartodb-popup header yellow v2">' +
-          '<a href="#close" class="cartodb-popup-close-button close">x</a>' +
-          '<div class="cartodb-popup-content-wrapper">' +
-            '<div class="cartodb-popup-content">' +
+        var popup_template = '<span>' +
               '<h4>Address:</h4>' +
-              '<p>{{address_1}}</p>' +
+              '<p>{{address}}</p>' +
               '<h4>Emissions:</h4>' +
-              '<p>{{direct_ghg_emissions_mtco2e}}</p>' +
+              '<p>{{total_ghg}}</p>' +
               '<h4>ID:</h4>' + 
               '<p>{{cartodb_id}}</p>' +
-              '<p><a onclick="myFoo()">Foo!</a></p>' +
-            '</div>' +
-          '</div>' +
-          '<div class="cartodb-popup-tip-container"></div>' +
-        '</div></span>';
-
-        console.log(infowindow_template);
+              '<p><button ng-click="compare()" ng-disabled="haveThree">Compare</button></p>' +
+              '</span>';
 
         // load map visualization
-        cartodb.createVis('mymap', 'http://azavea-demo.cartodb.com/api/v2/viz/701a2d94-44f2-11e4-bb22-0e10bcd91c2b/viz.json')
+        cartodb.createVis('mymap', 'http://azavea-demo.cartodb.com/api/v2/viz/c5a9af6e-7f12-11e4-8f24-0e018d66dc29/viz.json',
+                          {'infowindow': false})
             .done(function(vis, layers) {
                 console.log('map viz loaded!');
                 var nativeMap = vis.getNativeMap();
                 var overlay = layers[1];
-                console.log(nativeMap);
-                console.log(overlay);
 
-                var sublayer = overlay.getSubLayer(1);
-                sublayer.infowindow.set('template', infowindow_template);
+                // find the viz layer we want to interact with
+                var sublayer = overlay.getSubLayer(0);
+                sublayer.setInteraction(true);
+
+                // give user a pointy hand when they hover over a feature
+                sublayer.on('mouseover', function() {
+                    $('.leaflet-container').css('cursor','pointer');
+                });
+
+                // go back to grabby hand for panning when they mouse away
+                sublayer.on('mouseout', function() {
+                    $('.leaflet-container').css('cursor', '-webkit-grab');
+                    $('.leaflet-container').css('cursor', '-moz-grab');
+                });
 
                 sublayer.on('featureClick', function(e, latlng, pos, data, subLayerIndex) {
 
+                    // TODO: something to indicate popup is loading, hang on...
                     console.log('hey, that tickles!');
 
-                    // if we want to, can go asynchronously fetch stuff here / do other things
-
-                    
-                    var qry = 'SELECT cartodb_id, energy_star_score, address_1, direct_ghg_emissions_mtco2e, property_name ' + 
-                    'FROM mos_beb_2012 where cartodb_id = {{id}}';
+                    // Go fetch data for this feature; 'data' object from click method only has
+                    // the interactivity field, which is the cartodb_id.
+                    var qry = 'SELECT cartodb_id, geocode_address, total_ghg, property_name ' + 
+                    'FROM mos_beb_2013 where cartodb_id = {{id}}';
                     var sql = new cartodb.SQL({ user: 'azavea-demo'});
                     sql.execute(qry, { id: data.cartodb_id})
                         .done(function(data) {
                             var data = data.rows[0];
-                            console.log(data);
-                            $scope.current_popup_id = data.cartodb_id;
+                            $scope.cartodb_id = data.cartodb_id;
+                            $scope.address = data.geocode_address;
+                            $scope.total_ghg = data.total_ghg;
+
+                            var popup = $compile(popup_template)($scope);
+                            $scope.$apply(); // tell Angular to really, really go compile now
+
+                            L.popup({
+                                minWidth: 100
+                            }).setLatLng(latlng).setContent(popup[0]).openOn(nativeMap);
 
                         }).error(function(errors) {
                             // returns a list
                             console.log('errors: ' + errors);
                         });
-                    
-
                 });
-
-                console.log(sublayer.getSQL());
             });
-
-        // $timeout(function () { map.invalidateSize(); });
-
     }
 
     angular.module('mos.views.map')
     .controller('MapController', MapController);
 
 })();
-
-// onclick event for infowindow
-function myFoo() {
-    'use strict';
-
-    console.log('clicked infowindow');
-
-    // get reference to AngularJS scope
-    var scope = angular.element($("#mymap")).scope();
-
-    // can reference things on scope within apply function
-    scope.$apply(function() {
-        scope.msg = 'yee haw';
-        console.log(scope.current_popup_id);
-    });
-}
